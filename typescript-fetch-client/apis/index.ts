11464b72ca5e3d0c57aa8bd7fe2fe661a59a3616
YukiHara1126 <YukiHara1126@users.noreply.github.com>
2022-07-23 02:17:57 +0000

    copy target.yml

---
+++ typescript-fetch-client/apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BagsApi';
+export * from './CouponsApi';
+export * from './DefaultApi';
+export * from './EcApi';
+export * from './LoginApi';
+export * from './ProfileApi';
+export * from './RegisterApi';
+export * from './UsersApi';
